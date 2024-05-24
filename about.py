6e53a11e6663574cf5960d9914746aa7cb9635fc
kevin162001 <kevinjesseharyanto11@gmail.com>
2024-05-24 21:26:51 +0700

    Update about.py

--- about.py
+++ about.py
@@ -2,5 +2,5 @@ import streamlit as st
 def app():
     # Judul dan Informasi mengenai Dasboard
     st.markdown("""
-    <h1 style='text-align: center; color: black;'>tentang kami(synergisma) 🌦️</h1>
+    <h1 style='text-align: center; color: black;'>tentang kami(synergisma) </h1>
     """, unsafe_allow_html=True)
