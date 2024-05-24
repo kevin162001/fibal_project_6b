d509a50cf779fbe2cb2e6fcffb63e5b2865f3680
kevin162001 <kevinjesseharyanto11@gmail.com>
2024-05-24 23:46:25 +0700

    Update eda.py

--- eda.py
+++ eda.py
@@ -1,3 +1,7 @@
 import streamlit as st
 def app():
+    # Judul dan Informasi mengenai Dasboard
+    st.markdown("""
+    <h1 style='text-align: center; color: black;'>tentang kami(synergisma) </h1>
+    """, unsafe_allow_html=True)
   
